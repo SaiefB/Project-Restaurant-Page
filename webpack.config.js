2eda5d4be3cc1f58d2d2d77eef06d56495b0c584
Saief <A.S.Bokor@outlook.com>
2025-01-07 19:11:11 +0000

    setup

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,10 @@ module.exports = {
   },
   devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/template.html"],
+    static: path.resolve(__dirname, "dist"), // add this
+    watchFiles: ["src/template.html"],
+    hot: true, // Enable hot module replacement - add this
+    open: true, // Open browser on start - add this
   },
   plugins: [
     new HtmlWebpackPlugin({
